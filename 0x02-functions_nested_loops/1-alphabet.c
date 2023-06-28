45e2c6338bc5d976caef3d343edac20c21fc22d9
MissWilson981 <gracewilson981@gmail.com>
2023-06-28 13:12:07 -0700

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; la <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+}
