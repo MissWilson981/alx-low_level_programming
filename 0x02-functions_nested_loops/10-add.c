fa98c0f8487a88a26adab4f1dedef940e1b5057c
MissWilson981 <gracewilson981@gmail.com>
2023-06-28 22:54:26 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: check character a
+ * @b: check character b
+ * Return: c
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
